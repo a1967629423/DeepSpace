94b4785c7ce616b0b8a8c524c626c69dd4d90376
a1967629423 <1967629423@qq.com>
2018-10-23 12:56:16 +0800

    UI模块更新

---
+++ assets/Script/UI/MainEngine.ts
@@ -0,0 +1,42 @@
+import Character from "../Character";
+
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+    @property(Character)
+    player:Character = null;
+    @property(cc.ProgressBar)
+    bar:cc.ProgressBar = null;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+     update () {
+         if(this.player&&this.bar)
+         {
+            this.bar.progress = this.player.Engine/this.player.defEngine;
+         }
+     }
+}
