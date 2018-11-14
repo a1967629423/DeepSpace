246e3de262cc6682bf9ab2582056c090ba6184b7
a1967629423 <1967629423@qq.com>
2018-11-14 22:50:01 +0800

    优化代码结构

--- assets/Script4/State_Drag.ts
+++ assets/Script4/State_Drag.ts
@@ -14,7 +14,7 @@ export default class State_Drag extends CharacterState3 {
         this.nowRotato = this.character.node.rotation;
         console.log(this.nowRotato);
         this.firstTouch = this.character.firstTouchPosition;
-        setTimeout(()=>{if(this.character.body.type != cc.RigidBodyType.Static)this.character.body.type = cc.RigidBodyType.Static});
+        setTimeout(() => { if (this.character.body.type != cc.RigidBodyType.Static) this.character.body.type = cc.RigidBodyType.Static });
     }
     onTouchV2(v2: cc.Vec2) {
 
@@ -22,66 +22,57 @@ export default class State_Drag extends CharacterState3 {
         var rotation = this.lunchdir.signAngle(cc.v2(0, 1)) * 180 / Math.PI;
         this.character.node.rotation = rotation;
 
-        
+
 
     }
-    changeToLunch()
-    {
-        if(this.character.nowState!==this.character.LunchState)
-        {
-            this.character.lunchDirect = this.lunchdir.normalize();   
+    changeToLunch() {
+        if (this.character.nowState !== this.character.LunchState) {
+            this.character.lunchDirect = this.lunchdir.normalize();
             this.character.changeState(this.character.LunchState);
         }
     }
-    changeToIdel()
-    {
-        if(this.character.nowState!==this.character.IdleState)
-        {
+    changeToIdel() {
+        if (this.character.nowState !== this.character.IdleState) {
             this.character.node.rotation = this.nowRotato;
-            this.character.changeState(this.character.IdleState);  
+            this.character.changeState(this.character.IdleState);
         }
     }
     endTouch() {
-        if(this.character.nowWall)
-        {
-            if(this.lunchdir.y>=0)
-            {
-                if(this.character.nowWall.Type === WallType.Left&&this.lunchdir.x>0)
-                {
-                    this.changeToLunch();
-                } else if(this.character.nowWall.Type === WallType.Right&&this.lunchdir.x<0)
-                {
-                    this.changeToLunch();
-                } else
-                {
-                
-                    this.changeToIdel();
-                }
-            }
-            else
-            {
-                this.changeToIdel();
-            }
-        }
-        else if(this.lunchdir.y>0) //在地面上
-        {
-            this.changeToLunch();
-        }
-        else
-        {
-            this.changeToIdel();
-        }
         if (Until.isTouch(this.character.CancelNode)) {
 
             this.changeToIdel();
         }
+        else
+            if (this.character.nowWall) {
+                if (this.lunchdir.y >= 0) {
+                    if (this.character.nowWall.Type === WallType.Left && this.lunchdir.x > 0) {
+                        this.changeToLunch();
+                    } else if (this.character.nowWall.Type === WallType.Right && this.lunchdir.x < 0) {
+                        this.changeToLunch();
+                    } else {
+
+                        this.changeToIdel();
+                    }
+                }
+                else {
+                    this.changeToIdel();
+                }
+            }
+            else if (this.lunchdir.y > 0) //在地面上
+            {
+                this.changeToLunch();
+            }
+            else {
+                this.changeToIdel();
+            }
+
     }
     Quit() {
         this.firstTouch = cc.v2(0, 0);
         this.lunchdir = cc.v2(0, 0);
         this.character.firstTouchPosition = cc.v2(0, 0);
         //this.character.node.rotation = this.nowRotato;
-        
+
         this.nowRotato = 0;
         if (this.character.CancelNode) this.character.CancelNode.active = false;
     }
