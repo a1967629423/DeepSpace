2a80f206595b0f6ff675473ab7fcfc1b9502aab6
a1967629423 <1967629423@qq.com>
2018-11-18 16:20:57 +0800

    shader优化

--- assets/Material/Scroll.js
+++ assets/Material/Scroll.js
@@ -8,46 +8,32 @@ cc.Class({
     },
 
     // LIFE-CYCLE CALLBACKS:
-    setAll(f)
-    {
-        if(f)
-        {
-            var sprite = f.getComponent("cc.Sprite");
-            if(sprite)
-            {
-                sprite.setSpriteOtherMaterial(new OtherMaterial());
-            }
-            for(var a in f.children)
-            {
-                var sprite = f.children[a].getComponent("cc.Sprite");
-                if(sprite)
-                {
-                    sprite.setSpriteOtherMaterial(new OtherMaterial());
-                }
-                this.setAll(f.children[a]);
-            }
-        }
-    },
     onLoad () {
         cc.dynamicAtlasManager.enabled = false;
-        this._material2 = new OtherMaterial();
-        var f = cc.find("Canvas");
-        this.setAll(f);
+        this._material = new OtherMaterial();
         
     },
 
     start () {
-        this._start = Date.now();
         if (this.cocos) {
             //设置material
-            
-            this.cocos.setSpriteOtherMaterial(this._material2);
+            this._start = Date.now();
+            let texture = this.cocos.spriteFrame.getTexture();
+            //设置材质贴图
+            this._material.texture = texture;
+            //this._material.setResolution(this.cocos.node.width, this.cocos.node.height);
+            //刷新材质
+            this._material.updateHash();
+            //从新设置sprite材质
+            this.cocos._material = this._material;
+            //设置渲染数据
+            //TODO:优化shader管理
+            this.cocos._renderData._material = this._material;
         }
     },
-
     update (dt) {
         var addtime = Date.now()-this._start;
-        this._material2.uvOffset = addtime/1000;
+        this._material.uvOffset = addtime/1000;
         
     },
 });
